9e6de2f565698466ef713bb93cb6c5628a06eafe
AlaaMensh <princesslole464@yahoo.com>
2018-11-27 19:25:03 +0200

    palarandom

--- src/team_15/Team_15.java
+++ src/team_15/Team_15.java
@@ -3,19 +3,46 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package team_15;
+
+import java.util.Scanner;
 
 /**
  *
  * @author FUTURE
  */
 public class Team_15 {
-
+public static boolean ispala(String word)
+{
+    int i=0;
+    int j=word.length()-1;
+    while(j>i)
+    {
+        if(word.charAt(i)!=word.charAt(j))
+        { return false;}
+        i++;
+        j--;
+    }
+    return true;
+}
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("hello");
+        Scanner input=new Scanner(System.in);
+        String word=input.next();
+        boolean flag=ispala(word);
+        if(flag==true)
+        {
+            System.out.print("is pala");
+        }
+        else {
+            System.out.print("is not pala");
+        }
+        
+       
+        
         // TODO code application logic here
     }
     
