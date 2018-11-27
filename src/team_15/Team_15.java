eae4995b2826932b034f305356fc9c316907485d
AlaaMensh <princesslole464@yahoo.com>
2018-11-27 08:10:39 +0200

    Functions

---
+++ src/team_15/Team_15.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team_15;
+
+/**
+ *
+ * @author FUTURE
+ */
+public class Team_15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("hello");
+        // TODO code application logic here
+    }
+    
+}
